a133c7108d426c809249bed72da50b0cafdf9fb2
Jawad Khawaja <devjawkha@gmail.com>
2019-01-12 00:16:46 +0500

    add file loader to webpack

--- web-client/webpack.config.base.js
+++ web-client/webpack.config.base.js
@@ -18,6 +18,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
         exclude: /node_modules/
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
       }
     ]
   },
